1df30aa60b1afdf48da8038cfeec538eccf43c2c
Ian Pierce <ian@popularpays.com>
2020-02-07 14:12:04 -0600

    formatted number outputs

--- client/websocket.js
+++ client/websocket.js
@@ -1,3 +1,29 @@
+function Formatter(input) {
+    const formatter = Object.create(Formatter.prototype);
+    formatter.input = input;
+
+    return formatter;
+}
+
+Formatter.prototype.currency = function () {
+    return this.input.toFixed(2);
+};
+
+function Percentage(input) {
+    const percentage = Object.create(Percentage.prototype)
+    percentage.input = input;
+
+    return percentage;
+}
+
+Percentage.prototype.isPositive = function () {
+    return this.input > 0;
+};
+
+Percentage.prototype.toString = function () {
+    return `${Math.abs(this.input).toFixed(2)}%`;
+};
+
 const exampleSocket = new WebSocket("ws://localhost:2345");
 
 exampleSocket.onopen = function (event) {
@@ -8,10 +34,17 @@ exampleSocket.onmessage = function (event) {
     console.log(event.data);
 
     const screen_obj = JSON.parse(event.data);
-    document.getElementById("price").innerHTML = screen_obj.price;
+
+    if(screen_obj == null) {
+        return;
+    }
+
+    const percentChange = Percentage(screen_obj.net_percentage);
+
+    document.getElementById("price").innerHTML = Formatter(screen_obj.price).currency();
     document.getElementById("symbol").innerHTML = screen_obj.symbol;
     document.getElementById("currency-code").innerHTML = screen_obj.currency_code;
-    document.getElementById("net-percentage").innerHTML = screen_obj.net_percentage;
+    document.getElementById("net-percentage").innerHTML = percentChange.toString();
     document.getElementById("duration").innerHTML = screen_obj.duration;
 };
 exampleSocket.onerror = function (event) {
